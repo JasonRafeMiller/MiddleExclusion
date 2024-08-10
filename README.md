87e0c3322cf72faf3bc1c890bf54eda0b60898d7
JasonRafeMiller <jmill02@shepherd.edu>
2024-08-10 16:08:59 -0400

    Add docstrings and comments.

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 * [lncATLAS](https://lncatlas.crg.eu/) - Get lncATLAS_all_data_RCI.csv
 * lncATLAS_100.ipynb - Extract database info.
 
-## Part 2: GenCode
-* [GenCode](https://www.gencodegenes.org/)
+## Part 2: GENCODE
+* [GENCODE](https://www.gencodegenes.org/)
 * [FTP](https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human) - release 45
 * Sequences - gencode.v45.lncRNA_transcripts.fa.gz
 * Annotations - gencode.v45.long_noncoding_RNAs.gff3.gz
--- get_canonical_transcript_ids.45.sh
+++ get_canonical_transcript_ids.45.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Extract transcript IDs.
+# Input: the gff3 annotation file from GENCODE, gzipped.
+# Filters: 
+# - Require "Ensembl_canonical" i.e. the one best transcript to represent this gene
+# - Require column 3 say "transcript (to exclude "gene" and "exon")
+# - These filters also exclude comment lines, which start with #
+# Parse column 9
+# - Expect a list of tag=value pairs, delimited by semi-colon
+# - Expect "transcript_id" is not the last tag, so is followed by semi-colon
+# - Extract just the "transcript_id" tag and its value
+# - Extract just the value
+# Output: write one ID per line to a text file
+
 gunzip -c gencode.v45.long_noncoding_RNAs.gff3.gz | \
     grep Ensembl_canonical | \
     awk '{if ($3=="transcript") print $9;}' | \
